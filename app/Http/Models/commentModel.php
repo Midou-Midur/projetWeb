278b592af98269f376a7b7ba624692f61d0818de
EnzoB99 <38659859+EnzoB99@users.noreply.github.com>
2019-11-18 09:40:17 +0100

    Update commentModel.php

--- app/Http/Models/commentModel.php
+++ app/Http/Models/commentModel.php
@@ -1,11 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class commentModel extends Model
 {
-    protected $fillable=['comment'];
-    protected $hidden=['isPublic'];
+    public $timestamps = false;
+    
+    protected $fillable=['event_id', 'user_id', 'content', 'isPublic'] ;
+    
+    public function event_img(){
+        return $this->belongsTo('App\Models\Event_img');
+    }
+    
 }
