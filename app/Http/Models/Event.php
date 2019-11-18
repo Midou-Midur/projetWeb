206e6c6963da4572b4edd99293b931b4d1948174
EnzoB99 <38659859+EnzoB99@users.noreply.github.com>
2019-11-18 09:43:31 +0100

    Update and rename eventsModel.php to Event.php

---
+++ app/Http/Models/Event.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    public $timestamps = false;
+    
+    protected $fillable=['event_name', 'event_desc', 'event_date', 'event_price', 'isCurrent', 'isRecurrent', 'isPublic'];
+    
+    public function event_imgs(){
+        return $this->hasMany('App\Models\'Event_img');
+    }
+    
+}
