4e7bcc71d8bc0a0fdaf26582b7d83df36980acba
EnzoB99 <38659859+EnzoB99@users.noreply.github.com>
2019-11-18 11:57:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//dev
-use App\Http\Controller\AProposController;
-use App\Http\Controller\BoutiqueController;
-use App\Http\Controller\pastEventController;
-use App\Http\Controller\currentEventController;
 
 
-//Management et respect des règles
-use App\Http\Controller\cgvController;
-use App\Http\Controller\contactController;
-use App\Http\Controller\creditController;
-use App\Http\Controller\profilController;
-use App\Http\Controller\legalController;
-
-/*
-Pour y accéder depuis le navigateur :
-http://localhost/ProjetWebA2 - Copie/public/
-*/
 Route::get('/', function () {
     return view('pages.home');
 })->name('pages.home');
@@ -40,25 +24,31 @@ Route::get('/pages/logout', 'LoginController@logout')->name('pages.logout');
 
 //dev
 Route::prefix('pages')->
-group(function(){
-    Route::get('aPropos', 'aProposController')->name('pages.aPropos');
-    Route::get('boutique', 'boutiqueController')->name('pages.boutique');
+	group(function(){
+		Route::get('aPropos', 'aProposController')->name('pages.aPropos');
+		Route::get('boutique', 'ArticleController@index')->name('pages.boutique');
+		Route::post('addToCart', 'CartController@addToCart')->name('addToCart');
+		Route::get('cart', 'CartController@showCart')->name('cart');
+		Route::any('validation', 'MailController@send')->name('validation');
 
-    //Partie login
-    Route::get('account', 'UserController@accueil')->name('pages.account');
 
-    //Event
-    Route::get('pastEvent', 'pastEventController')->name('pages.pastEvent');
-    Route::get('currentEvent', 'currentEventController')->name('pages.currentEvent');
-    Route::get('addEvent', 'EventController@voirFormulaire')->name('pages.addEvent');
-    Route::post('addEvent', 'EventController@addEvent')->name('pages.addEvent');
-    Route::get('events/{id}', 'EventController@index')->name('pages.events');
+		//Partie login
+		Route::get('account', 'UserController@accueil')->name('pages.account');
 
-    //Management et respect des règles
-    Route::get('cgv', 'cgvController')->name('pages.cgv');
-    Route::get('contact', 'contactController')->name('pages.contact');
-    Route::get('credit', 'creditController')->name('pages.credit');
-    Route::get('profil', 'profilController')->name('pages.profil');
+		//Event
+		Route::get('pastEvent', 'pastEventController')->name('pages.pastEvent');
+		Route::get('currentEvent', 'currentEventController')->name('pages.currentEvent');
+		Route::get('addEvent', 'EventController@voirFormulaire')->name('pages.addEvent');
+		Route::post('addEvent', 'EventController@addEvent')->name('pages.addEvent');
+		Route::get('events/{id}', 'EventController@index')->name('pages.showEvent');
+		Route::get('events', 'EventController@index')->name('pages.events');
+		Route::get('eventMgmt', 'EventController@manage')->name('pages.eventMgmt');
+		
+		//Management et respect des règles
+		Route::get('cgv', 'cgvController')->name('pages.cgv');
+		Route::get('contact', 'contactController')->name('pages.contact');
+		Route::get('credit', 'creditController')->name('pages.credit');
+		Route::get('profil', 'profilController')->name('pages.profil');
 });
 
 Route::get('pages/addArticle', 'ArticleController@voirFormulaire')->name('pages.addArticle');
@@ -66,8 +56,11 @@ Route::post('pages/addArticle', 'ArticleController@create');
 
 
 Route::get('legal', function(){
-    return View::make('pages.legal')->compact('pages.legal');
-})->name('pages.legal');
+		return view('pages.legal');
+	})->name('pages.legal');
 
 Route::get('/pages/addUser', 'UserController@voirFormulaire')->name('pages.addUser');
 Route::post('/pages/addUser', 'UserController@addUser')->name('pages.addUser');
+
+Route::get('/account/dataTableEvents', 'UserController@dataTableEvents')->name('dataTableEvents');
+Route::get('account/dataTableArticles', 'UserController@dataTableArticles')->name('dataTableArticles');
